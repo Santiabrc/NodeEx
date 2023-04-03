3deb29a376c2ff359aa504eab8ded06d10028bbd
Santiago Nuñez <sjnunez@teco.com.ar>
2023-04-03 15:56:53 -0300

    fix imports and add function that pause the app

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,21 +1,23 @@
 import "colors";
-import { inquirerMenu }  from "../04-tareas-hacer/helpers/inquirer";
+import { inquirerMenu, pausa }  from "./helpers/inquirer.js";
+
 
 console.clear();
 
 const main = async() =>{
     console.log("hola perro");
+
     let opt = '';
 
     do{
-        opt = await inquirerMenu;
+        opt = await inquirerMenu();
         console.log({opt});
 
-       
+       await pausa();
 
     } while (opt !== '0');
 
-    // pausa();
+    
 }
 
 main();
--- 04-tareas-hacer/helpers/inquirer.js
+++ 04-tareas-hacer/helpers/inquirer.js
@@ -1,52 +1,65 @@
-import 'colors';
-import { prompt } from 'inquirer';
+import "colors";
+import inquirer from "inquirer";
 
 const preguntas = [
-    {
-        type: 'list',
-        name: 'opcion',
-        message: 'Que desea hacer?',
-        choices: [{
-            value: '1',
-            name: '1. Crear tarea'
-        },
-        {
-            value: '2',
-            name: '2. Listar tareas'
-        },
-        {
-            value: '3',
-            name: '3. Listar tareas completadas'
-        },
-        {
-            value: '4',
-            name: '4. Listar tareas pendientes'
-        },
-        {
-            value: '5',
-            name: '5. Completar tarea(s)'
-        },
-        {
-            value: '6',
-            name: '6. Borrar tarea'
-        },
-        {
-            value: '0',
-            name: '0. Salir'
-        },
-    ]
-    }
+  {
+    type: "list",
+    name: "opcion",
+    message: "Que deseas hacer muñeco?",
+    choices: [
+      {
+        value: "1",
+        name: "1. Crear tarea",
+      },
+      {
+        value: "2",
+        name: "2. Listar tareas",
+      },
+      {
+        value: "3",
+        name: "3. Listar tareas completadas",
+      },
+      {
+        value: "4",
+        name: "4. Listar tareas pendientes",
+      },
+      {
+        value: "5",
+        name: "5. Completar tarea(s)",
+      },
+      {
+        value: "6",
+        name: "6. Borrar tarea",
+      },
+      {
+        value: "0",
+        name: "0. Salir",
+      },
+    ],
+  },
 ];
 
-const inquirerMenu = async() => {
-    console.clear();
+const inquirerMenu = async () => {
+  console.clear();
 
-    console.log("=========================");
-    console.log("==Seleccione una opción==");
-    console.log("=========================\n");
+  console.log("=========================");
+  console.log("==Seleccione una opción==");
+  console.log("=========================\n");
 
-    const opt = await prompt(preguntas);
+  const { opcion } = await inquirer.prompt(preguntas);
 
-    return opt;
-}
-export default inquirerMenu; 
+  return opcion;
+};
+
+const pausa = async () => {
+
+    const sarasa = [{
+        type: 'input',
+        name: 'input',
+        message: `presione ${'enter'.green} para continuar`
+    }]
+    console.log(`\n`)
+  await inquirer.prompt(sarasa);
+};
+
+export { inquirerMenu, pausa };
