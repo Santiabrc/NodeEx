9807d6b4adb1107c746d743f9a208f8466db781a
Santiago Nuñez <sjnunez@teco.com.ar>
2023-05-11 17:35:01 -0300

    mapbox API conditioning

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,8 +1,40 @@
-import { leerInput } from "./helpers/inquirer.js";
+import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
+import { Busquedas } from "./models/busquedas.js";
 
 
 const main = async() => {
 
+    const busquedas = new Busquedas();
+
+    let opt;
+
+    do{
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                //mostrar mensaje
+                const lugar = await leerInput('Ciudad ')
+                await busquedas.ciudad(lugar)
+
+                console.log('\ninformación de la ciudad\n'.green)
+                console.log('Ciudad:')
+                console.log('Ciudad:', '')
+                console.log('Lat:')
+                console.log('Long')
+                console.log('temperatura')
+
+                break;
+        
+            default:
+                break;
+        }
+
+        console.log(opt)
+
+        if (opt !== 0) await pausa();
+
+    } while (opt !== 0)
 
 }
 
