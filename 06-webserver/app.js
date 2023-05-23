fa528bd276a8e6ff96b4d26ed31226f83f7c92d3
Santiago Nuñez <sjnunez@teco.com.ar>
2023-05-23 17:25:05 -0300

    add an entire webite to webServer

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -7,6 +7,12 @@ app.use(express.static('public'))
 app.get('/hola-mundo', (req, res)=> {
     res.send('Hello on hello-world route')
   })
+  app.get('/elements', (req, res) =>{
+    res.sendFile(__dirname + '/public/elements.html')
+})
+  app.get('/generic', (req, res) =>{
+    res.sendFile(__dirname + '/public/generic.html')
+})
 app.get('*', (req, res) =>{
     res.sendFile(__dirname + '/public/404.html')
 })
