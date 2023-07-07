534521594fd8a7aaf2be8f9ee8b58f92ce457f84
Santiago Nuñez <sjnunez@teco.com.ar>
2023-07-07 14:19:16 -0300

    add product categorie

---
+++ 07-restserver/models/index.js
@@ -0,0 +1,17 @@
+
+
+
+const Producto = require('./producto');
+const Role = require('./role');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+
+
+module.exports = {
+    Producto,
+    Role,
+    Server,
+    Usuario,
+}
+
