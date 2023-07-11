4a29e1aa3717f7cc8de0574ca1484e8489fc9eb8
Santiago Nuñez <sjnunez@teco.com.ar>
2023-07-11 12:37:24 -0300

    add 'on/off' messages to notify server status

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,1 +1,18 @@
-const socketClient = io()
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+const socketClient = io();
+
+socketClient.on('connect', () =>{
+    console.log('conectado')
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = ''; 
+});
+
+socketClient.on('disconnect', () =>{
+    console.log('Desconectado del servidor')
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none'; 
+});
