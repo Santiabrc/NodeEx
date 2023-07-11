f0c9f85006d34eba020cc5773194d60c3dee0212
Santiago Nuñez <sjnunez@teco.com.ar>
2023-07-11 13:20:42 -0300

    add config to listen on client side an server side

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,5 +1,7 @@
 const lblOnline = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
 
 const socketClient = io();
 
@@ -16,3 +18,14 @@ socketClient.on('disconnect', () =>{
     lblOffline.style.display = '';
     lblOnline.style.display = 'none'; 
 });
+
+btnEnviar.addEventListener('click', () =>{
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123abc',
+        fecha: new Date().getTime()
+    }
+
+    socketClient.emit('enviar-mensaje', payload);
+})
